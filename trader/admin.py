0994e8f52642eb78e3e6324c1006b430dff3dfe6
ze561384 <68448899+ze561384@users.noreply.github.com>
2021-03-30 14:33:51 -0500

    Admin user

--- trader/admin.py
+++ trader/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from trader.models import User
+from trader.models import Stock
+from trader.models import Transaction
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Stock)
+admin.site.register(Transaction)
