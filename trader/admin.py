9969eb9cd2d46802bcf4169013a5d37254e47fd6
ze561384 <68448899+ze561384@users.noreply.github.com>
2021-04-20 14:57:52 -0500

    Commit

--- trader/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from trader.models import User
-from trader.models import Stock
-from trader.models import Transaction
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Stock)
-admin.site.register(Transaction)
--- trader/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TraderConfig(AppConfig):
-    name = 'trader'
